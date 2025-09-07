3549cc47b3fadc6aa87b0475c74dfb850116d6c6
suspistew <3636719+suspistew@users.noreply.github.com>
2025-09-07 10:32:43 +0200

    ajout pages

---
+++ src/routes/drawings/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+<div class="home">
+    <div>
+        <div class="title">
+            <div class="title--name">Suspistew</div>
+            <div class="title--description">Admin console</div>
+        </div>
+        <div>
+            à gauche : listing articles
+            à droite : listing stats
+        </div>
+    </div>
+</div>
+
+<style>
+
+    .home {
+        height: 100%;
+        width: 500px;
+        margin: auto;
+        font-weight: 200;
+        font-size: 16px;
+        display: flex;
+        flex-direction: row;
+        vertical-align: center;
+        align-items: center;
+        justify-content: center;
+        text-align: left;
+    }
+
+    .home div {
+        width: 100%;
+    }
+
+    .title--name {
+        font-weight: 500;
+        font-size: 15px;
+    }
+
+    .title--description {
+        font-weight: 100;
+    }
+</style>
